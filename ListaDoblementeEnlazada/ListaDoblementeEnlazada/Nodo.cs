064de744009d55522c0a8ee0388c223391757853
Edgar Manuel García González <manolo161070@hotmail.com>
2014-10-16 09:45:08 -0500

    Clase Nodo

---
+++ ListaDoblementeEnlazada/ListaDoblementeEnlazada/Nodo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaDoblementeEnlazada
+{
+    class Nodo
+    {
+        public string dato; //Información
+        public Nodo sig; //Enlace con el siguiente nodo
+        public Nodo ant;
+        public Nodo(string dato)
+        {
+            this.dato = dato;
+            this.sig = null;
+            this.ant = null;
+        }
+
+        public Nodo(string dato,Nodo sig)
+        {
+            this.dato = dato;
+            this.sig = sig;
+            this.ant = ant;
+        }
+    }
+    }
+}
